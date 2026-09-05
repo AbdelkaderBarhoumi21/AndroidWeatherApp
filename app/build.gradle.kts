836228f01d56f3b0a9e57fc90d647dc01ec8b0b0
AbdelkaderBarhoumi21 <abdelkaderbarhoumi21@gmail.com>
2026-09-05 16:19:52 +0100

    `feat: add Retrofit, OkHttp, and serialization dependencies`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,14 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    // Http client
+    implementation(libs.retrofit)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging)
+
+    // Serializations
+    implementation(libs.serialization.json)
+    implementation(libs.retrofit.serialization)
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)

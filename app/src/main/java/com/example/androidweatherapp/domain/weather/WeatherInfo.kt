59926c873faf98aa7d70d83a2fd46496edee49c2
AbdelkaderBarhoumi21 <abdelkaderbarhoumi21@gmail.com>
2026-09-05 22:53:17 +0100

    `docs: add documentation for weatherDataPerDay in WeatherInfo`

--- app/src/main/java/com/example/androidweatherapp/domain/weather/WeatherInfo.kt
+++ app/src/main/java/com/example/androidweatherapp/domain/weather/WeatherInfo.kt
@@ -2,6 +2,7 @@ package com.example.androidweatherapp.domain.weather
 
 data class WeatherInfo(
     // Int  Key => current day index
+    // for example for today 1 it will have Map<1,List<WeatherData>>  List of 24 WeatherData : each item has 1  hour of WeatherData (1 day= 24 hour)
     val weatherDataPerDay:Map<Int,List<WeatherData>>,
     val currentWeatherData:WeatherData?,
 )
